41d7f4424d310c0058242cb2fd468005b4e5017f
7odaOmar10 <mahmoud202>
2024-04-21 22:42:14 +0200

    questions

---
+++ app/src/main/java/com/projects/germanlanguageapp/ui/admin/Questions/Choose/ChooseRecyclerView.kt
@@ -0,0 +1,18 @@
+package com.projects.germanlanguageapp.ui.admin.Questions.Choose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projects.germanlanguageapp.R
+
+class ChooseRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+
+
+
+
+}
